916fc6d6f0f2867935e0cd4e92f52eb44974b456
Eric Evans <eevans@sym-link.com>
2011-09-27 16:54:00 -0500

    burn a version to make GC's downloads happy

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from os.path import abspath, join, dirname
 
 setup(
     name="cql",
-    version="1.0.4",
+    version="1.0.5",
     description="Cassandra Query Language driver",
     long_description=open(abspath(join(dirname(__file__), 'README'))).read(),
     maintainer='Cassandra DBAPI-2 Driver Team',
